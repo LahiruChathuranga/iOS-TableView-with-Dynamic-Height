2af23d3bb8d600d6f25cc15c88d1c7afc0e5949a
Lahiru Chathuranga <lahiru@authnex.com>
2020-11-02 14:34:23 +0530

    completed

---
+++ TBVSample/InfoTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  InfoTVCell.swift
+//  TBVSample
+//
+//  Created by Lahiru Chathuranga on 11/2/20.
+//
+
+import UIKit
+
+class InfoTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TBVSample/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  TBVSample
+//
+//  Created by Lahiru Chathuranga on 11/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var heightCons: NSLayoutConstraint!
+    
+    
+    var infoArray: [String] = []
+    let cellIdentifire = "InfoTVCell"
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        addTempData()
+    }
+    
+    func addTempData() {
+        let arraySize = 100
+        
+        for _ in 0..<arraySize {
+            let str = randomString(length: 10)
+            infoArray.append(str)
+        }
+        
+        if infoArray.isEmpty {
+            tableView.isHidden = true
+        } else {
+            tableView.isHidden = false
+            print(infoArray.count)
+            self.heightCons.constant = CGFloat((self.infoArray.count * 50))
+            self.tableView.reloadData()
+           
+            
+            
+        }
+    }
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return infoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifire, for: indexPath) as? InfoTVCell {
+            cell.textLabel?.text = infoArray[indexPath.row]
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50.0
+    }
+    
+}
+extension ViewController {
+    func randomString(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+}
